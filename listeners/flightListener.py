de192fa17e1bef3c13ccdf87ef64941dce1c09fe
Axel MACIEJEWSKI <axel.maciejewski@viacesi.fr>
2020-05-26 13:52:34 +0200

    fix(statuses): added new events
    TODO:
    Create a new communication between listener <=> drone for alerts and some events (e.g. Go Home, prioritize)
    Add new maps between GO <=> Python
    Add a new pipe listener for the Diagnostic tool (and possible actions)

--- listeners/flightListener.py
+++ listeners/flightListener.py
@@ -14,7 +14,42 @@ from olympe.messages.ardrone3.PilotingState import (
     NavigateHomeStateChanged,
     moveToChanged
 )
+
+from olympe.enums.ardrone3.PilotingState import (
+    AlertStateChanged_State,
+    FlyingStateChanged_State,
+    NavigateHomeStateChanged_State,
+)
 from olympe.messages.ardrone3.GPSSettingsState import HomeChanged, GPSFixStateChanged
+
+flying_status_lookup = (
+    FlyingStateChanged_State.landed,
+    FlyingStateChanged_State.takingoff,
+    FlyingStateChanged_State.flying,
+    FlyingStateChanged_State.landing,
+    FlyingStateChanged_State.emergency,
+    FlyingStateChanged_State.usertakeoff,
+    FlyingStateChanged_State.motor_ramping,
+    FlyingStateChanged_State.emergency_landing
+)
+
+alert_lookup = (
+    AlertStateChanged_State.none,
+    AlertStateChanged_State.user,
+    AlertStateChanged_State.cut_out,
+    AlertStateChanged_State.critical_battery,
+    AlertStateChanged_State.low_battery,
+    AlertStateChanged_State.too_much_angle,
+    AlertStateChanged_State.almost_empty_battery 	
+)
+
+navigate_home_status_lookup = (
+    NavigateHomeStateChanged_State.available,
+    NavigateHomeStateChanged_State.inProgress,
+    NavigateHomeStateChanged_State.unavailable,
+    NavigateHomeStateChanged_State.pending
+)
+
 #endregion OLYMPE
 
 # FlightListener Regroupe tous les états intéressants envoyés par le drone PARROT / AR.SDK
@@ -44,25 +79,38 @@ class FlightListener(olympe.EventListener):
 
     @olympe.listen_event(NavigateHomeStateChanged())
     def on_navigation_changed(self, event, scheduler):
+        global navigate_home_status_lookup
         try :
             if hasattr(self, "my_log"):
                 self.my_log.info("{} = {}".format(event.message.name, event.args["state"]))
-            # TODO : Implémenter la logique du OnNavigateHome
+            self.emit_status("navigate_home_status_changed", navigate_home_status_lookup.index(event.args["state"]))
         except(Exception) as err :
             if hasattr(self, "my_log"):
                 self.my_log.info(err)
            
 
-    @olympe.listen_event(FlyingStateChanged() | AlertStateChanged() )
+    @olympe.listen_event(FlyingStateChanged())
     def on_state_changed(self, event, scheduler):
+        global flying_status_lookup
         try :
             if hasattr(self, "my_log"):
                 self.my_log.info("{} = {}".format(event.message.name, event.args["state"]))
-            self.on_internal_status_changed(event.message.name, event.args["state"])
+            self.emit_status("flight_status_changed", flying_status_lookup.index(event.args["state"]))
         except(Exception) as err :
             if hasattr(self, "my_log"):
                 self.my_log.info(err)
              
+
+    @olympe.listen_event(AlertStateChanged() )
+    def on_alert_raised(self, event, scheduler):
+        global alert_lookup
+        try :
+            if hasattr(self, "my_log"):
+                self.my_log.info("{} = {}".format(event.message.name, event.args["state"]))
+            self.emit_status("on_alert_changed", alert_lookup.index(event.args["state"]))
+        except(Exception) as err :
+            if hasattr(self, "my_log"):
+                self.my_log.info(err)
 
     @olympe.listen_event(PositionChanged() | moveToChanged())
     def on_position_changed(self, event, scheduler):
@@ -92,6 +140,7 @@ class FlightListener(olympe.EventListener):
             self._relay.update_position(event.args["latitude"], event.args["longitude"], event.args["altitude"])
             self.on_element_position_update('home_position_update', event)
 
+    """
     @olympe.listen_event(
         FlyingStateChanged(state="motor_ramping")
         >> FlyingStateChanged(state="takingoff", _timeout=1.)
@@ -100,7 +149,9 @@ class FlightListener(olympe.EventListener):
     def on_take_off(self, event, scheduler):
         if hasattr(self, "my_log"):
             self.my_log.info("Décollage du drone")
-        self.on_internal_status_changed(event.message.name, event.args["success"])
+        #event.message.name
+        self.emit_status("takeoff", event.args["success"])
+    """
 
     @olympe.listen_event(rssi_changed())
     def on_signal_changed(self, event, scheduler):
@@ -145,5 +196,15 @@ class FlightListener(olympe.EventListener):
         except(Exception) as error :
             if hasattr(self, "my_log"):
                 self.my_log.error("Une erreur est survenue lors de la mise à jour des positions")
+                self.my_log.error(error)
+                self.my_log.error("{} = {}".format(value.message.name, value.args))
+
+    def emit_status(self, name, value):
+        try :
+            if hasattr(self, "_socket_initialized") and hasattr(self, "my_name") and hasattr(self,"_brain_client") and self._socket_initialized:
+                self._brain_client.emit(name, {"drone_name":self.my_name, "status":value})
+        except(Exception) as error :
+            if hasattr(self, "my_log"):
+                self.my_log.error("Une erreur est survenue lors de la mise à jour des états interne")
                 self.my_log.error(error)
                 self.my_log.error("{} = {}".format(value.message.name, value.args))
